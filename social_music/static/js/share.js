b0dab8d86ea23988308429a0e15c941a51dd8ad2
JiayaoZhou <jraffry@163.com>
2015-05-25 14:17:46 +0800

    xia-la-geng-xin

--- social_music/static/js/share.js
+++ social_music/static/js/share.js
@@ -204,4 +204,12 @@ window.onload = function () {
 
     }
 }
+    window.onscroll=function(){
+        var a = document.documentElement.clientHeight;
+        var b = document.documentElement.scrollTop==0? document.body.scrollTop : document.documentElement.scrollTop;
+        var c = document.documentElement.scrollHeight;
+        if(a+b==c)
+            alert('a:'+a+'b:'+b+'c:'+c);
+    }
+    
 
